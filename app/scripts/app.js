60e599e2a3bf10599a237da8fd0b3332d597c274
Christopher McCloud <mccloud.christopher@gmail.com>
2014-08-28 17:44:34 -0700

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name gitWorkflowPracticeApp
+ * @description
+ * # gitWorkflowPracticeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('gitWorkflowPracticeApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
